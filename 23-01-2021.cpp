5b8fbbaa004c5d7e3d84a92fe47d7c10decde61a
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-01-22 06:19:58 +0530

    Create 23-01-2021.cpp

---
+++ 23-01-2021.cpp
@@ -0,0 +1,39 @@
+/*
+Question - 
+  You will be given the coordinates of four points A, B, C and X.
+  You have to find whether the Point X lies inside the triangle formed by the points A, B and C or not.
+  For Example: 
+    Input:
+    0 0 10 0 0 10 2 2
+    Output: 
+    1
+*/
+
+// SOLUTION - 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Point{
+    public:
+        int x, y;
+};
+
+float area(int x1, int y1, int x2, int y2, int x3, int y3) 
+{ 
+   return abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0); 
+} 
+
+int main()
+{
+    Point p, q, r, s;
+    cin >> p.x >> p.y >> q.x >> q.y >> r.x >> r.y >> s.x >> s.y;
+    float bigArea = area(p.x, p.y, q.x, q.y, r.x, r.y);
+    float area1 = area(p.x, p.y, q.x, q.y, s.x, s.y);
+    float area2 = area(p.x, p.y, r.x, r.y, s.x, s.y);
+    float area3 = area(q.x, q.y, r.x, r.y, s.x, s.y);
+    if(area1 + area2 + area3 == bigArea)
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+}
