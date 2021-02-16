59b3841cf7233bb200709960cdafac730d5f7096
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-02-16 23:31:34 +0530

    Create Is it a VOWEL or CONSONANT.py

---
+++ codechef/Is it a VOWEL or CONSONANT.py
@@ -0,0 +1,13 @@
+# Question Link - https://www.codechef.com/problems/VOWELTB
+
+# Solution Link - https://www.codechef.com/viewsolution/36888742
+
+# SOLUTION - 
+
+# cook your dish here
+x = input()
+vowels = "AEIOU"
+if x in vowels:
+    print("Vowel")
+else:
+    print("Consonant")
