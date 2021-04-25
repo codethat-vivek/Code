3977fec7b32137505ecc047967e843baa88cfb37
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-04-25 14:21:47 +0530

    Create CHN09.cpp

---
+++ codechef/CHN09.cpp
@@ -0,0 +1,29 @@
+// Question Link - https://www.codechef.com/problems/CHN09
+
+// Solution Link - https://www.codechef.com/viewsolution/36868177
+
+// SOLUTION - 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	string s;
+	while(t--){
+	    cin >> s;
+	    int cnt_a = 0, cnt_b = 0;
+	    int i;
+	    for(i=0; s[i] != '\0'; i++){
+	        if(s[i] == 'a')
+	            cnt_a ++;
+	        else
+	            cnt_b ++;
+	    }
+	    cout << min(cnt_a, cnt_b) << "\n";
+	}
+	return 0;
+}
