7417f167d48a5eaac3e831c13a9bd0b4011d42d0
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-04-25 14:23:36 +0530

    Create Area OR Perimeter.cpp

---
+++ codechef/Area OR Perimeter.cpp
@@ -0,0 +1,23 @@
+// Question Link - https://www.codechef.com/problems/AREAPERI
+
+// Solution Link - https://www.codechef.com/viewsolution/36889830
+
+// SOLUTION - 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int l, b;
+	cin >> l >> b;
+	int area = l*b;
+	int perimeter = 2*(l+b);
+	if(area > perimeter)
+	    cout << "Area" << "\n" << area;
+	else if(area < perimeter)
+	    cout << "Peri" << "\n" << perimeter;
+	else
+	    cout << "Eq" << "\n" << area;
+	return 0;
+}
