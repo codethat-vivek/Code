63fddacb893fcde3187e70203adab316842b3798
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-06-14 06:24:22 +0530

    Create A kcal.cpp

---
+++ Daily/AtCoder Beginner Contest205/A kcal.cpp
@@ -0,0 +1,14 @@
+// Question Link - https://atcoder.jp/contests/abc205/tasks/abc205_a
+
+// Solution - 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a, b;
+  cin >> a >> b;
+  
+  cout << (a * b)/100.0 ;
+  
+}
