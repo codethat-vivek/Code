1ae6e04d0c21c08a86e022b1763d860201b72f75
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-06-19 02:13:13 +0530

    Create Pascal's Triangle II.cpp

---
+++ LeetCode/Pascal's Triangle II.cpp
@@ -0,0 +1,21 @@
+// Question Link - https://leetcode.com/problems/pascals-triangle-ii/
+
+// Solution - 
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        if(rowIndex == 0)
+            return {1};
+        if(rowIndex == 1)
+            return {1, 1};
+        vector<int>ans(rowIndex + 1, 1);
+        for(int i=0; i<rowIndex-1; i++){
+            for(int j=i+1; j>=1; j--){
+                ans[j] += ans[j-1];
+            }
+        }
+        return ans;
+        
+    }
+};
