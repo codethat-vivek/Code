e45ca241028c6ed44836b3f7f8a0bdeef3799abb
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-06-29 20:16:21 +0530

    Create SelectionSort.cpp

---
+++ SortingAlgorithms/SelectionSort.cpp
@@ -0,0 +1,43 @@
+/*
+
+SELECTION SORT  ->
+                  Time Complexity(Best) - O(n^2)
+                  Time Complexity(Worst) - O(n^2)
+                  Space Complexity - O(1)
+                  In-Place - Yes
+                  Stable - No
+                  Online - No
+                  
+                  
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+vector<ll> selectionSort(ll n, vector<ll>arr){
+    int mn, idx;
+    for(int i=0; i<n-1; i++){
+        idx = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[idx] > arr[j])
+                idx = j;
+            
+        }
+        swap(arr[i], arr[idx]);
+    }
+    return arr;
+}
+
+int main()
+{
+    vector<ll>arr = {1, 4, 2, 36, 6, 7, 65, 3, 25, 45, 8, 9, 20, 3, 6, 0};
+    vector<ll>ans = selectionSort(arr.size(), arr);
+
+    for(auto x : ans){
+        cout << x << " ";
+    }
+
+    return 0;
+}
