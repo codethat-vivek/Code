afbde2b3c094a2453f0d49dae6c15cea7f4611d7
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-07-09 14:45:10 +0530

    Create Find First Missing Number.cpp

---
+++ Daily/Find First Missing Number.cpp
@@ -0,0 +1,18 @@
+/*
+Question - Given an array that consists of positive integers. Find the first integer that is missing in the given array.
+Sample Input -
+[2, 5, 1, 4]
+Output -
+3
+
+Sample Input -
+[4, 6, 1, 2, 54, 88, 3]
+Output - 
+5
+*/
+
+
+// Solution - 
+
+
+
