bbbe5624af8221d8239eff0b4bd102e99777791b
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-07-16 23:45:50 +0530

    Update and rename 18-01-2021.py to Maximum Toys.py

--- Daily/Maximum Toys.py
+++ Daily/Maximum Toys.py
@@ -1,5 +1,5 @@
 '''
-Question -
+Question - Very easy problem
   Mark and Jane are very happy after having their first child. Their son loves toys, so Mark wants to buy some. 
   There are a number of different toys lying in front of him, tagged with their prices. Mark has only a certain amount to spend, 
   and he wants to maximize the number of toys he buys with this money. Given a list of toy prices and an amount to spend, 
