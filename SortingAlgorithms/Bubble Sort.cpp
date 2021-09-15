e96c1e083d25bfb158e6bebac84e3cfeef384325
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-09-15 16:29:41 +0530

    Create Bubble Sort.cpp

---
+++ SortingAlgorithms/Bubble Sort.cpp
@@ -0,0 +1,39 @@
+  
+/*
+BUBBLE SORT  ->
+                  Time Complexity(Best) - O(n) # when already sorted
+                  Time Complexity(Worst) - O(n^2)
+                  Space Complexity - O(1)
+                  In-Place - Yes
+                  Stable - Yes
+                  Online - No
+                  
+                  
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+vector<ll> bubbleSort(ll n, vector<ll>arr){
+    for (int i = 0; i < n-1; i++){     
+        for (int j = 0; j < n-i-1; j++){ 
+            if (arr[j] > arr[j+1]) 
+                swap(arr[j], arr[j+1]);
+        }
+    }
+    return arr;
+}
+
+int main()
+{
+    vector<ll>arr = {1, 4, 2, 36, 6, 7, 65, 3, 25, 45, 8, 9, 20, 3, 6, 0};
+    vector<ll>ans = bubbleSort(arr.size(), arr);
+
+    for(auto x : ans)
+        cout << x << " ";
+    
+
+    return 0;
+}
