8f84c375e4f607b0b777d9b38649e3f6a4f099f0
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-10-04 17:43:21 +0530

    Create Min Stack.cpp

---
+++ LeetCode/Min Stack.cpp
@@ -0,0 +1,43 @@
+// Question Link - https://leetcode.com/problems/min-stack/
+
+// Solution - 
+
+class MinStack {
+public:
+    stack<pair<int, int>>st;
+    
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        int minElement = INT_MAX;
+        if(st.empty())
+            st.push({val, val});
+        else{
+            minElement = min(st.top().second, val);
+            st.push({val, minElement});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
