c9aad2d8bef3e5a70d5b3a79946d2880612285de
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-10-10 19:00:22 +0530

    Update Bubble Sort.cpp

--- SortingAlgorithms/Bubble Sort.cpp
+++ SortingAlgorithms/Bubble Sort.cpp
@@ -17,8 +17,8 @@ using namespace std;
 #define ll long long
 
 vector<ll> bubbleSort(ll n, vector<ll>arr){
-    for (int i = 0; i < n-1; i++){     
-        for (int j = 0; j < n-i-1; j++){ 
+    for (ll i = 0; i < n-1; i++){     
+        for (ll j = 0; j < n-i-1; j++){ 
             if (arr[j] > arr[j+1]) 
                 swap(arr[j], arr[j+1]);
         }
@@ -31,7 +31,7 @@ int main()
     vector<ll>arr = {1, 4, 2, 36, 6, 7, 65, 3, 25, 45, 8, 9, 20, 3, 6, 0};
     vector<ll>ans = bubbleSort(arr.size(), arr);
 
-    for(auto x : ans)
+    for(ll x : ans)
         cout << x << " ";
     
 
