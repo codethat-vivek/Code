265cc11386e8d8adb6f22847fca0a12ecb7e632e
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-10-21 14:28:16 +0530

    Create Insert Delete GetRandom O(1).py

---
+++ LeetCode/Insert Delete GetRandom O(1).py
@@ -0,0 +1,31 @@
+# Question Link - https://leetcode.com/problems/insert-delete-getrandom-o1/
+
+# Solution - 
+
+class RandomizedSet:
+    
+    def __init__(self):
+        self.st = set()
+
+    def insert(self, val: int) -> bool:
+        if val in self.st: 
+            return False
+        self.st.add(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val in self.st: 
+            self.st.remove(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        from random import choice
+        return choice(list(self.st))
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
