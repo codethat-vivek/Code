f435c5fff14ba253512a7fb3735df0c9a397d0df
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2021-10-29 19:06:59 +0530

    Create Count Nice Pairs in an ArrayCount Nice Pairs in an Array.py

---
+++ LeetCode/Count Nice Pairs in an ArrayCount Nice Pairs in an Array.py
@@ -0,0 +1,20 @@
+# Question Link - https://leetcode.com/problems/count-nice-pairs-in-an-array/
+
+# Solution - 
+
+inf = int(1e9+7)
+class Solution:
+        
+    def countNicePairs(self, nums: List[int]) -> int:
+        from math import comb
+        from collections import Counter
+        diff = []
+        for i in nums:
+            rev = int(str(i)[::-1])
+            diff.append(i-rev)
+        #print(diff)
+        count = Counter(diff)
+        ans = 0
+        for each in count:
+            ans = ( ans + comb(count[each], 2) ) % inf
+        return ans
