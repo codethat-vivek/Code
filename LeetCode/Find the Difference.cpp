687889bafb20248e02aa61b22ef8c766710f8126
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2022-02-07 17:13:12 +0530

    Create Find the Difference.cpp

---
+++ LeetCode/Find the Difference.cpp
@@ -0,0 +1,16 @@
+// Question Link - https://leetcode.com/problems/find-the-difference/
+
+// Solution - 
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i=0; i<s.length(); i++){
+            if(s[i] != t[i])
+                return t[i];
+        }
+        return t[t.length()-1];
+    }
+};
