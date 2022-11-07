4b815c0f6afd61a996c5d839f25e2e1ab6b1ca01
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2022-11-07 13:07:22 +0530

    Create Maximum 69 Number.cpp

---
+++ LeetCode/Maximum 69 Number.cpp
@@ -0,0 +1,19 @@
+// Question Link - https://leetcode.com/problems/maximum-69-number/
+
+// Solution - 
+
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == '6'){
+                s[i] = '9';
+                break;
+            }
+        }
+        int x = stoi(s);
+        return x;
+    }
+};
