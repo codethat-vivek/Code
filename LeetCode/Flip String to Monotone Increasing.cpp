32eefcf5a66094078ae490e7b9353a45dc9bf7cc
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2023-01-18 01:43:40 +0530

    Update Flip String to Monotone Increasing.cpp

--- LeetCode/Flip String to Monotone Increasing.cpp
+++ LeetCode/Flip String to Monotone Increasing.cpp
@@ -6,44 +6,19 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n = s.length();
-        int zero[n], ones[n];
         int total_zeroes = 0;
         for(int i=0; i<n; i++){
             if(s[i] == '0')
                 total_zeroes++;
         }
-        if(s[0] == '0'){
-            zero[0] = 1;
-            ones[0] = 0;
-        }
-        else{
-            ones[0] = 1;
-            zero[0] = 0;
-        }
-        for(int i=1; i<n; i++){
-            if(s[i] == '0'){
-                zero[i] = zero[i-1] + 1;
-                ones[i] = ones[i-1];
-            }
-            else{
-                ones[i] = ones[i-1] + 1;
-                zero[i] = zero[i-1];
-            }
-        }
-        
-        // for(int i=0; i<n; i++)
-        //     cout << zero[i] << " ";
-        // cout << "\n";
-        // for(int i=0; i<n; i++)
-        //     cout << ones[i] << " ";
-        // cout << "\n";
-        
+        int ones = 0, zeroes = 0;
         int ans = min(n, total_zeroes);
         for(int i=0; i<n; i++){
             if(s[i] == '0')
-                ans = min(ans, ones[i]+total_zeroes-zero[i]);
+                zeroes++;
             else
-                ans = min(ans, ones[i]+total_zeroes-zero[i]);
+                ones++;
+            ans = min(ans, ones+total_zeroes-zeroes);
         }
         return ans;
     }
