9e02a52394ccf6db9e3a7b751ba3c432f3a289d5
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2024-03-09 23:13:06 +0530

    Create Minimum Common Value.cpp

---
+++ LeetCode/Minimum Common Value.cpp
@@ -0,0 +1,20 @@
+// Question Link - https://leetcode.com/problems/minimum-common-value/
+
+// Solution - 
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size(), n=nums2.size();
+        int i=0, j=0;
+        while(i<m && j<n){
+            if(nums1[i] == nums2[j])
+                return nums1[i];
+            else if(nums1[i] > nums2[j])
+                j++;
+            else
+                i++;
+        }
+        return -1;
+    }
+};
