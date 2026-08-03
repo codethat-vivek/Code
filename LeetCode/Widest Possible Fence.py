489415b05b92ff8e15c26e589c8efbe01795bb89
Vivek Kumar <70913416+codethat-vivek@users.noreply.github.com>
2026-08-03 05:38:20 -0400

    Update Widest Possible Fence.py

--- LeetCode/Widest Possible Fence.py
+++ LeetCode/Widest Possible Fence.py
@@ -7,23 +7,17 @@ class Solution:
         dt, dt2 = Counter(planks), Counter()
         n = len(planks)
         keys = sorted(list(dt.keys()))
-        # print(dt)
         for i in range(len(keys)):
             for j in range(i+1, len(keys)):
                 if (keys[i] + keys[j]) not in dt2:
                     dt2[keys[i] + keys[j]] += min(dt[keys[i]], dt[keys[j]]) + dt[keys[i]+keys[j]]
                 else:
                     dt2[keys[i] + keys[j]] += min(dt[keys[i]], dt[keys[j]])
-                
-        # print(dt2)
         
         for key in dt:
             if key not in dt2:
                 dt2[key] = dt[key]
-        finalDt = Counter(dt2)
-        # keys = sorted(list(dt.keys()))
         for key in keys:
-            if finalDt[2*key] > 0:
-                finalDt[2*key] += dt[key]//2
-        # print(finalDt)
-        return max(finalDt.values())
+            if dt2[2*key] > 0:
+                dt2[2*key] += dt[key]//2
+        return max(dt2.values())
